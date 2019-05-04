07691895f79c0848427b391ca5eeb4c6418d9036
WilliamBork33 <williambork33@gmail.com>
2019-05-04 11:26:00 -0500

    Heroku push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
  return res.send('/');
 });
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
